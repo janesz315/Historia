4035635ba14c8c4e2c23eaf86c639e3671932ae5
janesz315 <borosuveg6@gmail.com>
2025-02-19 09:45:33 +0100

    update userTest kész

--- backend/app/Http/Controllers/UserTestController.php
+++ backend/app/Http/Controllers/UserTestController.php
@@ -68,9 +68,33 @@ class UserTestController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateUserTestRequest $request, UserTest $userTest)
+    public function update(UpdateUserTestRequest $request, $id)
     {
-        //
+        $row = UserTest::find($id);
+        if ($row) {
+
+            try {
+                $row->update($request->all());
+                $data = [
+                    'message' => 'ok',
+                    'data' => $row
+                ];
+            } catch (\Illuminate\Database\QueryException $e) {
+                $data = [
+                    'message' => 'The patch failed',
+                    'data' => $request->all()
+                ];
+            }
+
+        } else {
+            $data = [
+                'message' => 'Not found',
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        }
+        return response()->json($data, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
