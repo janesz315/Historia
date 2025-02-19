deae805f011e2bbe2ca2c33ecf3542fa6241977a
janesz315 <borosuveg6@gmail.com>
2025-02-19 10:03:49 +0100

    create testquestion ok

--- backend/app/Http/Controllers/TestQuestionController.php
+++ backend/app/Http/Controllers/TestQuestionController.php
@@ -27,30 +27,98 @@ class TestQuestionController extends Controller
      */
     public function store(StoreTestQuestionRequest $request)
     {
-        //
+        try {
+            $row = TestQuestion::create($request->all());
+            $data = [
+                'message' => 'ok',
+                'data' => $row
+            ];
+        } catch (\Illuminate\Database\QueryException $e) {
+            $data = [
+                'message' => 'The post failed',
+                'data' => $request->all()
+            ];
+        }
+
+        return response()->json($data, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(TestQuestion $testQuestion)
+    public function show(int $id)
     {
-        //
+        $row = TestQuestion::find($id);
+        if ($row) {
+            $data = [
+                'message' => 'ok',
+                'data' => $row
+            ];
+        } else {
+            $data = [
+                'message' => 'Not found',
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        }
+        return response()->json($data, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTestQuestionRequest $request, TestQuestion $testQuestion)
+    public function update(UpdateTestQuestionRequest $request, int $id)
     {
-        //
+        $row = TestQuestions::find($id);
+        if ($row) {
+
+            try {
+                $row->update($request->all());
+                $data = [
+                    'message' => 'ok',
+                    'data' => $row
+                ];
+            } catch (\Illuminate\Database\QueryException $e) {
+                $data = [
+                    'message' => 'The patch failed',
+                    'data' => $request->all()
+                ];
+            }
+
+        } else {
+            $data = [
+                'message' => 'Not found',
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        }
+        return response()->json($data, options: JSON_UNESCAPED_UNICODE);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(TestQuestion $testQuestion)
+    public function destroy(int $id)
     {
-        //
+        $row = TestQuestions::find($id);
+        if ($row) {
+            $row->delete();
+            $data = [
+                'message' => 'ok',
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        } else {
+            $data = [
+                'message' => 'Not found',
+                'data' => [
+                    'id' => $id
+                ]
+            ];
+        }
+        return response()->json($data, options: JSON_UNESCAPED_UNICODE);
     }
 }
