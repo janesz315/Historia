75baa5c619ee51a7c1ee51117f4401e3d1b0f094
janesz315 <borosuveg6@gmail.com>
2025-03-06 13:50:02 +0100

    A kezdetleges oldalcímek megoldva

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from "@/stores/useAuthStore.js";
+
+
+function checkIfNotLogged() {
+  const storeAuth = useAuthStore();
+  if (!storeAuth.user) {
+    return "/login";
+  } else {
+    
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +19,7 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {title: (route) => 'Kezdőlap'}
     },
     {
       path: '/about',
@@ -16,8 +28,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
+      meta: {title: (route) => 'Rólunk'}
     },
   ],
 })
 
+router.beforeEach((to, from, next) => {
+  const title = to.meta.title;
+  document.title ="Historia - " + to.meta.title(to);
+  next();
+})
 export default router
