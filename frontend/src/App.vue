68182f6379d72dc382dd0f37940dec92862f949b
Eren0414 <kritof.jager.2006@gmail.com>
2025-04-29 01:48:46 +0200

    menü jobb oldalra igazítása félig kész

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,177 +2,93 @@
   <div class="my-container">
     <!-- Fő fejléc -->
     <header>
-      <div class="wrapper container-fluid">
-        <!-- Logo / Weboldal neve -->
+      <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: #8b5a2b">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
 
-        <!-- Navigációs menü -->
-        <nav
-          class="navbar navbar-expand-lg navbar-dark fixed-top"
-          style="background-color: #8b5a2b"
-        >
-          <div class="logo">
+          <!-- Logo / Weboldal neve -->
+          <div class="navbar-brand logo">
             <RouterLink to="/">Historia</RouterLink>
           </div>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+
+          <!-- Hamburger menü -->
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
+
+          <!-- Navigációs menü -->
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto d-flex justify-content-end">
               <li class="nav-item">
-                <RouterLink to="/" class="nav-link" @click="closeNavbar"
-                  >Kezdőlap</RouterLink
-                >
+                <RouterLink to="/" class="nav-link" @click="closeNavbar">Kezdőlap</RouterLink>
               </li>
               <li class="nav-item">
-                <RouterLink to="/rolunk" class="nav-link" @click="closeNavbar"
-                  >Rólunk</RouterLink
-                >
+                <RouterLink to="/rolunk" class="nav-link" @click="closeNavbar">Rólunk</RouterLink>
               </li>
 
               <!-- Admin -->
-              <li
-                v-if="stateAuth.user && stateAuth.roleId === 1"
-                class="nav-item"
-              >
-                <RouterLink
-                  to="/temakorokAdmin"
-                  class="nav-link"
-                  @click="closeNavbar"
-                  >Témakörök</RouterLink
-                >
+              <li v-if="stateAuth.user && stateAuth.roleId === 1" class="nav-item">
+                <RouterLink to="/temakorokAdmin" class="nav-link" @click="closeNavbar">Témakörök</RouterLink>
               </li>
-              <li
-                v-if="stateAuth.user && stateAuth.roleId === 2"
-                class="nav-item"
-              >
-                <RouterLink
-                  to="/temakorok"
-                  class="nav-link"
-                  @click="closeNavbar"
-                  >Témakörök</RouterLink
-                >
+              <li v-if="stateAuth.user && stateAuth.roleId === 2" class="nav-item">
+                <RouterLink to="/temakorok" class="nav-link" @click="closeNavbar">Témakörök</RouterLink>
               </li>
 
               <li v-if="stateAuth.user" class="nav-item">
-                <RouterLink to="/tesztek" class="nav-link" @click="closeNavbar"
-                  >Tesztek</RouterLink
-                >
+                <RouterLink to="/tesztek" class="nav-link" @click="closeNavbar">Tesztek</RouterLink>
               </li>
 
               <!-- User -->
 
               <!-- Admin -->
-              <li
-                v-if="stateAuth.user && stateAuth.roleId === 1"
-                class="nav-item"
-              >
-                <RouterLink to="/admin" class="nav-link" @click="closeNavbar"
-                  >Admin</RouterLink
-                >
+              <li v-if="stateAuth.user && stateAuth.roleId === 1" class="nav-item">
+                <RouterLink to="/admin" class="nav-link" @click="closeNavbar">Admin</RouterLink>
               </li>
 
-              <li
-                v-if="stateAuth.user && stateAuth.roleId === 1"
-                class="nav-item"
-              >
-                <RouterLink
-                  to="/kerdestipusok"
-                  class="nav-link"
-                  @click="closeNavbar"
-                  >Kérdéstípusok</RouterLink
-                >
+              <li v-if="stateAuth.user && stateAuth.roleId === 1" class="nav-item">
+                <RouterLink to="/kerdestipusok" class="nav-link" @click="closeNavbar">Kérdéstípusok</RouterLink>
               </li>
 
-              <li
-                v-if="stateAuth.user && stateAuth.roleId === 1"
-                class="nav-item"
-              >
-                <RouterLink to="/kerdesek" class="nav-link" @click="closeNavbar"
-                  >Kérdésbank</RouterLink
-                >
+              <li v-if="stateAuth.user && stateAuth.roleId === 1" class="nav-item">
+                <RouterLink to="/kerdesek" class="nav-link" @click="closeNavbar">Kérdésbank</RouterLink>
               </li>
 
               <!-- Bejelentkezés és regisztráció -->
               <li v-if="!stateAuth.user" class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="userDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
                   <i class="bi bi-person"></i>
                 </a>
-                <ul
-                  class="dropdown-menu dropdown-menu-end"
-                  aria-labelledby="userDropdown"
-                >
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                   <li>
-                    <RouterLink
-                      to="/bejelentkezes"
-                      class="dropdown-item"
-                      @click="closeNavbar"
-                      >Bejelentkezés</RouterLink
-                    >
+                    <RouterLink to="/bejelentkezes" class="dropdown-item" @click="closeNavbar">Bejelentkezés
+                    </RouterLink>
                   </li>
                   <li>
-                    <RouterLink
-                      to="/regisztracio"
-                      class="dropdown-item"
-                      @click="closeNavbar"
-                      >Regisztráció</RouterLink
-                    >
+                    <RouterLink to="/regisztracio" class="dropdown-item" @click="closeNavbar">Regisztráció</RouterLink>
                   </li>
                 </ul>
               </li>
 
               <!-- Dropdown menü a felhasználónévvel -->
               <li v-if="stateAuth.user" class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="userDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
+                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
                   <i class="bi bi-person"></i> {{ stateAuth.user }}
                 </a>
-                <ul
-                  class="dropdown-menu dropdown-menu-end"
-                  aria-labelledby="userDropdown"
-                >
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
                   <li>
-                    <RouterLink
-                      class="dropdown-item"
-                      to="/profil"
-                      @click="closeNavbar"
-                      >Profil</RouterLink
-                    >
+                    <RouterLink class="dropdown-item" to="/profil" @click="closeNavbar">Profil</RouterLink>
                   </li>
                   <li>
-                    <RouterLink
-                      class="dropdown-item"
-                      to="/"
-                      @click="LogoutAndCloseNavbar()"
-                      >Kijelentkezés</RouterLink
-                    >
+                    <RouterLink class="dropdown-item" to="/" @click="LogoutAndCloseNavbar()">Kijelentkezés</RouterLink>
                   </li>
                 </ul>
               </li>
             </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
 
     <!-- Dinamikus tartalom megjelenítése -->
@@ -244,14 +160,16 @@ export default {
 
 /* Dropdown menü testreszabása */
 .dropdown-menu {
-  background-color: #8b5a2b; /* Az oldal háttérszíne */
+  background-color: #8b5a2b;
+  /* Az oldal háttérszíne */
   border: 2px solid white;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 }
 
 /* Dropdown menüpontok stílusa */
 .dropdown-item {
-  color: #ffffff; /* Fehér szöveg */
+  color: #ffffff;
+  /* Fehér szöveg */
   font-size: 16px;
   padding: 10px 15px;
   transition: background-color 0.3s ease;
@@ -259,13 +177,15 @@ export default {
 
 /* Hover hatás a menüpontokon */
 .dropdown-item:hover {
-  background-color: #a06b3c; /* Egy világosabb barna az egér fölé vitelekor */
+  background-color: #a06b3c;
+  /* Egy világosabb barna az egér fölé vitelekor */
   color: white;
 }
 
 /* A dropdown nyílának (caret) testreszabása */
 .navbar .dropdown-toggle::after {
-  border-top-color: white; /* Fehér nyíl */
+  border-top-color: white;
+  /* Fehér nyíl */
 }
 
 .my-container {
@@ -280,7 +200,8 @@ export default {
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  background-color: #8b5a2b; /* Sötét kékeszöld háttér */
+  background-color: #8b5a2b;
+  /* Sötét kékeszöld háttér */
 }
 
 .logo a {
@@ -307,8 +228,15 @@ nav.navbar {
   margin-left: 20px;
 }
 
+.navbar-collapse {
+  flex-grow: 1;
+  display: flex !important;
+  justify-content: center;
+}
+
 .nav-link {
-  color: #ecf0f1; /* Világos szürke szín */
+  color: #ecf0f1;
+  /* Világos szürke szín */
   text-decoration: none;
   font-size: 16px;
   padding: 8px 12px;
@@ -317,23 +245,28 @@ nav.navbar {
 }
 
 .nav-link:hover {
-  background-color: #3498db; /* Kék szín hover esetén */
+  background-color: #3498db;
+  /* Kék szín hover esetén */
   color: white;
 }
 
 /* Mobil reszponzív menü */
 @media (max-width: 768px) {
+
   /* Az alapértelmezett menü el van rejtve, és csak a hamburger ikon jelenik meg */
   .navbar-collapse {
-    display: none;
+    display: flex !important;
+    justify-content: center;
   }
 
   .navbar-toggler {
-    display: block; /* Hamburger ikon */
+    display: block;
+    /* Hamburger ikon */
   }
 
   .navbar-collapse.show {
-    display: block; /* A menü akkor jelenik meg, ha a hamburger ikonra kattintanak */
+    display: block;
+    /* A menü akkor jelenik meg, ha a hamburger ikonra kattintanak */
   }
 
   .navbar-nav {
